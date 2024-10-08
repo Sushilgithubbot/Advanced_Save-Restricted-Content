e1a6e6c94214c53a6b7c413121355982b0ad7cbf
Sushilgithubbot <sushilgithubbot@gmail.com>
2024-10-08 20:08:02 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -19,6 +19,6 @@ async def start(_, message):
     join = await subscribe(_, message)
     if join == 1:
         return
-    await message.reply_photo(photo="https://graph.org/Personal-Test-Bot-2-08-10",
+    await message.reply_photo(photo="https://graph.org/Advanced-Bot-10-08",
                               caption=script.START_TXT.format(message.from_user.mention), 
                               reply_markup=buttons)
